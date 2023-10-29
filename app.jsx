7ef0e8a9405cb8f9ec3b1b1add2e6a4306c9b72a
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2023-10-29 23:07:43 +0700

    first commit d:29102023

---
+++ app.jsx
@@ -0,0 +1,23 @@
+const app = document.getElementById("app")
+
+const AppContent = () => {
+    return(
+        <div>
+            <h1>Victor Crest</h1>
+            
+            26
+            London
+
+            80K
+            Followers
+
+            803K
+            Likes
+
+            1.4K
+            Photos
+        </div>
+    )
+}
+
+ReactDOM.render(<AppContent />, app)
