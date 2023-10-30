83b098575dee51c8e721d6127138259c327f3faa
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2023-10-30 18:40:07 +0700

    finish html template in jsx

--- app.jsx
+++ app.jsx
@@ -3,21 +3,38 @@ const app = document.getElementById("app")
 const AppContent = () => {
     return(
         <div>
-            <h1>Victor Crest</h1>
+            <div className="image-container">
+                <ImageComponent class="img-profile" src="images/image-victor.jpg" alt="profile-image" />
+            </div>
+            <div className="info-container">
+                <MainTitle title="Victor Crest" />
+                <Paragraph content="26" />
+                <Paragraph content="London" />
+            </div>
+            <hr />
+            <div className="sns-container">
+                <Paragraph content="80K" />
+                <Paragraph content="Followers" />
+                <Paragraph content="803K" />
+                <Paragraph content="Likes" />
+                <Paragraph content="1.4K" />
+                <Paragraph content="Photos" />
+            </div>
             
-            26
-            London
-
-            80K
-            Followers
-
-            803K
-            Likes
-
-            1.4K
-            Photos
         </div>
     )
+}
+
+function ImageComponent (props) {
+    return <img className={props.class} src={props.src} alt={props.altName} />
+}
+
+function Paragraph (props) {
+    return <p>{props.content}</p>
+}
+
+function MainTitle (props) {
+    return <h1>{props.title}</h1>
 }
 
 ReactDOM.render(<AppContent />, app)
