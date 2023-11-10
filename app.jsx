e5ccc0437f9b15e3d933c093a7e6602d26461f91
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2023-11-10 16:30:13 +0700

    Edit some files to comply with HTML validation d:10112023

--- app.jsx
+++ app.jsx
@@ -4,10 +4,10 @@ const AppContent = () => {
     return(
         <div className="container">
             <svg className="pattern top" width="978" height="978" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="a"><stop stopColor="#0989B4" stopOpacity="0" offset="0%"/><stop stopColor="#53FFEE" offset="99.94%"/></linearGradient></defs><ellipse fill="url(#a)" transform="rotate(-135 489 489)" cx="489" cy="489" rx="489" ry="488" fillRule="evenodd" opacity=".5"/></svg>
-            <svg className="pattern bottom" width="978" height="978" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="a"><stop stopColor="#0989B4" stopOpacity="0" offset="0%"/><stop stopColor="#53FFEE" offset="99.94%"/></linearGradient></defs><ellipse fill="url(#a)" transform="scale(1 -1) rotate(45 1669.55 0)" cx="489" cy="489" rx="489" ry="488" fillRule="evenodd" opacity=".25"/></svg>
+            <svg className="pattern bottom" width="978" height="978" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="b"><stop stopColor="#0989B4" stopOpacity="0" offset="0%"/><stop stopColor="#53FFEE" offset="99.94%"/></linearGradient></defs><ellipse fill="url(#a)" transform="scale(1 -1) rotate(45 1669.55 0)" cx="489" cy="489" rx="489" ry="488" fillRule="evenodd" opacity=".25"/></svg>
             <div className="profile-container">
                 <div className="image-container">
-                    <ImageComponent class="img-profile" src="images/image-victor.jpg" alt="profile-image" />
+                    <ImageComponent class="img-profile" src="images/image-victor.jpg" altName="profile-image" />
                 </div>
                 <div className="info-container">
                     <div className="group-1">
